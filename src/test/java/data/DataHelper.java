26162eadb27a72eeea6ab0c863753a24912923a2
Rraaalf <kymyass@gmail.com>
2024-06-26 21:23:02 +0300

    changed: исправлены локаторы; удалён избыточный метод getOtherAuthInfo

--- src/test/java/data/DataHelper.java
+++ src/test/java/data/DataHelper.java
@@ -8,14 +8,14 @@ public class DataHelper {
 
     @Value
     public static class AuthInfo {
-        private String login;
-        private String password;
+        String login;
+        String password;
 
     }
 
     @Value
     public static class VerificationCode {
-        private String code;
+        String code;
     }
 
     @Value
@@ -25,10 +25,6 @@ public class DataHelper {
 
     public static AuthInfo getAuthInfo() {
         return new AuthInfo("vasya", "qwerty123");
-    }
-
-    public static AuthInfo getOtherAuthInfo(AuthInfo original) {
-        return new AuthInfo("petya", "123qwerty");
     }
 
     public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
