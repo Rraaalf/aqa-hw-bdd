dad60aa73a39366cf6cf8ffe2cad71db332f85cd
Rraaalf <kymyass@gmail.com>
2024-06-26 21:25:39 +0300

    changed: тесты перевода с одной карты на другую и обратно поделены на два отдельных

--- src/test/java/test/MoneyTransferTest.java
+++ src/test/java/test/MoneyTransferTest.java
@@ -1,55 +1,72 @@
 package test;
 
 import com.codeborne.selenide.Configuration;
-import dev.failsafe.internal.util.Assert;
+import data.DataHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import data.DataHelper;
 import org.openqa.selenium.chrome.ChromeOptions;
-import page.LoginPageV2;
+import page.LoginPage;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
+import static data.DataHelper.getFirstCardInfo;
+import static data.DataHelper.getSecondCardInfo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
 
     @BeforeEach
     void setup() {
         ChromeOptions options = new ChromeOptions();
-        Map<String, Object> prefs = new HashMap<String, Object>();
+        Map<String, Object> prefs = new HashMap<>();
         prefs.put("credentials_enable_service", false);
         prefs.put("password_manager_enable", false);
         options.setExperimentalOption("prefs", prefs);
         Configuration.browserCapabilities = options;
-        Configuration.holdBrowserOpen = true;
     }
 
     @Test
-    void shouldTransferMoneyBetweenOwnCardsV2() {
-        open("http://localhost:9999");
-        var loginPage = new LoginPageV2();
+    void shouldTransferMoneyFromFirstToSecondCard() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
         var dashboardPage = verificationPage.validVerify(verificationCode);
-        var transferInfo = DataHelper.getCardTransferInfo("1000");
-        var transferPage = dashboardPage.transferToCard(transferInfo.getId1());
-        transferPage.addMoneyToCard1(transferInfo);
-        var actualBalance1 = dashboardPage.getCardBalance(transferInfo.getId1());
-        var expectedBalance1 = 11000;
-        Assert.isTrue(actualBalance1 == expectedBalance1, "Ошибка баланса карты 1");
-        var actualBalance2 = dashboardPage.getCardBalance(transferInfo.getId2());
-        var expectedBalance2 = 9000;
-        Assert.isTrue(actualBalance2 == expectedBalance2, "Ошибка баланса карты 2");
-        dashboardPage.transferToCard(transferInfo.getId1()).cleanUp();
-        dashboardPage.transferToCard(transferInfo.getId2()).addMoneyToCard2(transferInfo);
-        var actualBalance3 = dashboardPage.getCardBalance(transferInfo.getId1());
-        var expectedBalance3 = 10000;
-        Assert.isTrue(actualBalance3 == expectedBalance3, "Ошибка баланса карты 1");
-        var actualBalance4 = dashboardPage.getCardBalance(transferInfo.getId1());
-        var expectedBalance4 = 10000;
-        Assert.isTrue(actualBalance4 == expectedBalance4, "Ошибка баланса карты 2");
+        var firstCardInfo = getFirstCardInfo();
+        var secondCardInfo = getSecondCardInfo();
+        int amount = 10000;
+        var expectedBalance1 = dashboardPage.getCardBalance(firstCardInfo) - amount;
+        var expectedBalance2 = dashboardPage.getCardBalance(secondCardInfo) + amount;
+        var transferPage = dashboardPage.transferToCard(secondCardInfo);
+        dashboardPage = transferPage.addMoneyToCard(String.valueOf(amount), firstCardInfo);
+        var actualBalance1 = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalance2 = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalance1, actualBalance1);
+        assertEquals(expectedBalance2, actualBalance2);
+        closeWebDriver();
+    }
+
+    @Test
+    void shouldTransferMoneyFromSecondToFirstCard() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        var secondCardInfo = getSecondCardInfo();
+        var firstCardInfo = getFirstCardInfo();
+        int amount = 10000;
+        var expectedBalance2 = dashboardPage.getCardBalance(secondCardInfo) - amount;
+        var expectedBalance1 = dashboardPage.getCardBalance(firstCardInfo) + amount;
+        var transferPage = dashboardPage.transferToCard(firstCardInfo);
+        dashboardPage = transferPage.addMoneyToCard(String.valueOf(amount), secondCardInfo);
+        var actualBalance1 = dashboardPage.getCardBalance(secondCardInfo);
+        var actualBalance2 = dashboardPage.getCardBalance(firstCardInfo);
+        assertEquals(expectedBalance1, actualBalance1);
+        assertEquals(expectedBalance2, actualBalance2);
+        closeWebDriver();
     }
 }
